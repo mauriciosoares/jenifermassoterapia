db3d054eda55ab98f72f7325148f913e2d92f368
Mauricio Soares <m.soares2603@gmail.com>
2020-12-30 18:08:53 -0300

    add dynamic prices

---
+++ scripts.js
@@ -0,0 +1,21 @@
+$(function() {
+  $('.product-time-options a').on('click', function(e) {
+    e.preventDefault()
+    const $el = $(e.target)
+    const $parent = $(e.target).closest('.product')
+
+    $parent.find('.product-time-options a').removeClass('selected')
+    $el.addClass('selected')
+
+    const price = $el.data('price')
+    const time = $el.data('time')
+
+    if (price) {
+      $parent.find('.product-price span').html(price)
+    }
+
+    if (time) {
+      $parent.find('.product-time span').text(time)
+    }
+  })
+})
