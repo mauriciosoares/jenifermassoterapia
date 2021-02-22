0f3b9b1e612b249e9c6c68454b1d0ce35a901f80
Mauricio Soares <m.soares2603@gmail.com>
2021-02-21 21:04:22 -0300

    tons of other improvements

--- scripts.js
+++ scripts.js
@@ -1,28 +1,33 @@
-$(function() {
-  $('.product-time-options a').on('click', function(e) {
+const links = document.querySelectorAll('.product-time-options a')
+
+links.forEach(link => {
+  link.addEventListener('click', (e) => {
     e.preventDefault()
-    const $el = $(e.target)
-    const $parent = $(e.target).closest('.product')
-    const $priceTime = $parent.find('.product-price-time')
 
-    $parent.find('.product-time-options a').removeClass('selected')
-    $el.addClass('selected')
+    const el = e.target
+    const parent = el.closest('.product')
+    const priceTime = parent.querySelector('.product-price-time')
 
-    const price = $el.data('price')
-    const time = $el.data('time')
+    const allLinks = parent.querySelectorAll('.product-time-options a')
+    allLinks.forEach(link => {
+      link.classList.remove('selected')
+    })
+    el.classList.add('selected')
+
+    const price = el.getAttribute('data-price')
+    const time = el.getAttribute('data-time')
 
     if (price) {
-      $parent.find('.product-price span').html(price)
+      parent.querySelector('.product-price span').innerHTML = price
     }
 
     if (time) {
-      $parent.find('.product-time span').text(time)
+      parent.querySelector('.product-time span').textContent = time
     }
 
-    console.log($priceTime)
-    $priceTime.addClass('blink')
+    priceTime.classList.add('blink')
     setTimeout(function() {
-      $priceTime.removeClass('blink')
+      priceTime.classList.remove('blink')
     },200)
   })
 })
